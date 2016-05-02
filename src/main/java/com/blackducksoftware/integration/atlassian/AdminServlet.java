02827a19529ecf179af32325a5b472fdadc3500d
jrichard <jrichard@blackducksoftware.com>
2016-05-02 18:17:51 -0400

    Working through atlassian refapp tutorial to create the common config

---
+++ src/main/java/com/blackducksoftware/integration/atlassian/AdminServlet.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2 only
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License version 2
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *******************************************************************************/
+package com.blackducksoftware.integration.atlassian;
+
+import java.io.IOException;
+import java.net.URI;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.user.UserManager;
+import com.atlassian.templaterenderer.TemplateRenderer;
+
+public class AdminServlet extends HttpServlet {
+
+	private final UserManager userManager;
+	private final LoginUriProvider loginUriProvider;
+	private final TemplateRenderer renderer;
+
+	public AdminServlet(final UserManager userManager, final LoginUriProvider loginUriProvider,
+			final TemplateRenderer renderer) {
+		this.userManager = userManager;
+		this.loginUriProvider = loginUriProvider;
+		this.renderer = renderer;
+	}
+
+	@Override
+	public void doGet(final HttpServletRequest request, final HttpServletResponse response)
+			throws IOException, ServletException {
+		final String username = userManager.getRemoteUsername(request);
+		if (username == null || !userManager.isSystemAdmin(username)) {
+			redirectToLogin(request, response);
+			return;
+		}
+
+		response.setContentType("text/html;charset=utf-8");
+		renderer.render("admin.vm", response.getWriter());
+	}
+
+	private void redirectToLogin(final HttpServletRequest request, final HttpServletResponse response)
+			throws IOException {
+		response.sendRedirect(loginUriProvider.getLoginUri(getUri(request)).toASCIIString());
+	}
+
+	private URI getUri(final HttpServletRequest request) {
+		final StringBuffer builder = request.getRequestURL();
+		if (request.getQueryString() != null) {
+			builder.append("?");
+			builder.append(request.getQueryString());
+		}
+		return URI.create(builder.toString());
+	}
+}
---
+++ src/main/resources/js/hub-atlassian-config.js
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2 only
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License version 2
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *******************************************************************************/
+(function ($) { // this closure helps us keep our variables to ourselves.
+// This pattern is known as an "iife" - immediately invoked function expression
+ 
+    // form the URL
+    var url = AJS.contextPath() + "/rest/hub-admin/1.0/";
+ 
+    // wait for the DOM (i.e., document "skeleton") to load. This likely isn't necessary for the current case,
+    // but may be helpful for AJAX that provides secondary content.
+    $(document).ready(function() {
+        // request the config information from the server
+        $.ajax({
+            url: url,
+            dataType: "json"
+        }).done(function(config) { // when the configuration is returned...
+            // ...populate the form.
+            $("#name").val(config.name);
+            $("#time").val(config.time);
+        });
+    });
+
+})(AJS.$ || jQuery);
